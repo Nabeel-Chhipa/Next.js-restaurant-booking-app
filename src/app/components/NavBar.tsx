1219777018663267a2c77caaaf52693bbab2a933
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-09-06 00:43:56 +0500

    add routing

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,12 +1,11 @@
-import React from "react";
+import Link from "../../../node_modules/next/link";
 
 export default function NavBar() {
   return (
     <nav className="bg-white p-2 flex justify-between">
-      <a href="" className="font-bold text-gray-700 text-2xl">
-        {" "}
-        OpenTable{" "}
-      </a>
+      <Link href="/" className="font-bold text-gray-700 text-2xl">
+        OpenTable
+      </Link>
       <div>
         <div className="flex">
           <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
--- src/app/search/components/Header.tsx
+++ src/app/search/components/Header.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { useRouter } from "../../../../node_modules/next/navigation";
 
 export default function Header() {
+  const [data, setData] = useState("");
+  const router = useRouter();
+
   return (
     <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
       <div className="text-left text-lg py-3 m-auto flex justify-center">
@@ -8,8 +14,15 @@ export default function Header() {
           className="rounded  mr-3 p-2 w-[450px]"
           type="text"
           placeholder="State, city or town"
+          value={data}
+          onChange={(e) => setData(e.target.value)}
         />
-        <button className="rounded bg-red-600 px-9 py-2 text-white">
+        <button
+          className="rounded bg-red-600 px-9 py-2 text-white"
+          onClick={() =>
+            data == "kfc" ? router.push("/restaurant/kfc") : alert("Not Found")
+          }
+        >
           Let's go
         </button>
       </div>
