a80481ab92306bab49956697a0b5f7c017ca912a
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-09-08 00:03:29 +0500

    add layout file

--- src/app/reserve/[slug]/page.tsx
+++ src/app/reserve/[slug]/page.tsx
@@ -1,21 +1,15 @@
-import NavBar from "@/app/components/NavBar";
 import React from "react";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
 const page = () => {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
   );
 };
 
