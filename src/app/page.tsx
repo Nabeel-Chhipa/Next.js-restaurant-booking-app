d77f6c992c67eaaf5382aa342f7d3323c652c786
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-09-11 00:48:50 +0500

    integrate prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch('http://worldtimeapi.org/api/timezone/Asia/Karachi', {
+    next: {
+      revalidate: 5
+    }
+  })
+  const data = await res.json()
+
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        <p>{data.datetime}</p>
         <Card />
       </div>
     </main>
