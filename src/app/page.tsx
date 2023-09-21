4d9273fe587a4b392b4c67c996764f8416eced26
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-09-21 12:23:19 +0500

    fetch restaurants data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,45 @@
+import { Location, PRICE, PrismaClient, Region } from "@prisma/client";
 import Card from "./components/Card";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-import styles from "./page.module.css";
+
+export interface RestaurantCardType {
+  id: number
+  name: string
+  main_image: string
+  location: Location
+  region: Region
+  price: PRICE
+  slug: string
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  let restaurant = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      location: true,
+      region: true,
+      price: true,
+      slug: true,
+    }
+  });
+  return restaurant;
+};
 
 export default async function Home() {
-
-  const res = await fetch('http://worldtimeapi.org/api/timezone/Asia/Karachi', {
-    next: {
-      revalidate: 5
-    }
-  })
-  const data = await res.json()
+  const restaurants = await fetchRestaurants();
+  console.log(restaurants);
 
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <p>{data.datetime}</p>
-        <Card />
+        {restaurants.map((restaurant) => {
+          return <Card restaurant={restaurant} />;
+        })}
       </div>
     </main>
   );
