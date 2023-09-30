a0c21449de2c0646a3817e99c91f69c9db93ec15
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-09-30 17:48:35 +0500

    add restaurant menu page

--- src/app/restaurant/[slug]/menu/page.tsx
+++ src/app/restaurant/[slug]/menu/page.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import RestaurantMenu from "../components/RestaurantMenu";
 import RestaurantNavBar from "../components/RestaurantNavBar";
+import { PrismaClient } from "@prisma/client";
 
-const page = () => {
+const prisma = new PrismaClient
+
+const getRestaurantMenu = async (slug: string) => {
+  const menu = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      items: true
+    }
+  })
+
+  if(!menu) {
+    throw new Error
+  }
+
+  return menu.items
+}
+
+const page = async ({params}: {params: {slug: string}}) => {
+
+  const items = await getRestaurantMenu(params.slug)
+
+
   return (
     <>
       <div className="bg-white w-[100%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <RestaurantMenu />
+        <RestaurantNavBar slug={params.slug} />
+        <RestaurantMenu items={items}/>
       </div>
     </>
   );
